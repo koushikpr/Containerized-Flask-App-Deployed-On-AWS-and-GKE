b5471e18c18a71d203b1f35b61cf16a1719e22e1
koushikpr <karanboltz@gmail.com>
2023-06-15 16:08:23 +0530

    MongoClient Added

--- app.py
+++ app.py
@@ -1,26 +1,33 @@
 from flask import Flask, render_template, request, url_for, redirect
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
+from pymongo import MongoClient
 
 app = Flask(__name__)
 
-#Provide Your Mongo Database URI
-app.config['MONGO_URI'] = "mongodb://localhost:27017/" 
-mongo = PyMongo(app)
 
-todos = mongo.db
+#Provide your MongoDB Container name here instead of flaskapp-db-1
+client = MongoClient("flaskapp-db-1",27017)
 
-@app.route('/')
-def index():
-    saved_todos = todos.find()
-    return render_template('index.html', todos=saved_todos)
+todos = client.db.todos
 
+
+#CREATE
 @app.route('/add', methods=['POST'])
 def add_todo():
     new_todo = request.form.get('new-todo')
     todos.insert_one({'text' : new_todo, 'complete' : False})
     return redirect(url_for('index'))
 
+
+#READ
+@app.route('/')
+def index():
+    saved_todos = todos.find()
+    return render_template('index.html', todos=saved_todos)
+
+
+#UPDATE
 @app.route('/complete/<oid>')
 def complete(oid):
     todo_item = todos.find_one({'_id': ObjectId(oid)})
@@ -28,12 +35,20 @@ def complete(oid):
     todos.save(todo_item)
     return redirect(url_for('index'))
 
+
+#DELETE
 @app.route('/delete_completed')
 def delete_completed():
     todos.delete_many({'complete' : True})
     return redirect(url_for('index'))
 
+#DELETE ALL
 @app.route('/delete_all')
 def delete_all():
     todos.delete_many({})
     return redirect(url_for('index'))
+
+
+#INITIALIZATION
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', debug=True)
